24fd2794b247c26edc289b372725b75cf7a82467
kimutaiamos <kyle.kibet@student.moringaschool.com>
2021-11-29 18:25:36 +0300

    Initializes the buttons and the text-view

--- app/src/main/java/com/moringaschool/mycalculator/MainActivity.java
+++ app/src/main/java/com/moringaschool/mycalculator/MainActivity.java
@@ -3,12 +3,116 @@ package com.moringaschool.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1, button2, button3, button4, button5, button6, button7, button8, button9, buttonPoint, buttonC, button0,
+    buttonadd, buttonsub, buttoneql, buttonmul, buttondiv;
+    TextView textView_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        buttonPoint = (Button) findViewById(R.id.buttonPoint);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonadd = (Button) findViewById(R.id.buttonadd);
+        buttonsub = (Button) findViewById(R.id.buttonsub);
+        buttonmul = (Button) findViewById(R.id.buttonmul);
+        buttondiv = (Button) findViewById(R.id.buttondiv);
+        buttoneql = (Button) findViewById(R.id.buttoneql);
+        textView_result = (TextView) findViewById(R.id.textView_result);
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("0");
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("1");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("2");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("3");
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("4");
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("5");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("6");
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("1");
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("1");
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("1");
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("1");
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("1");
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView_result.setText("1");
+            }
+        });
     }
 }
